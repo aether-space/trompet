4e4a449581d63a4171eba91df9bde9d0e883fb35
Andreas Stührk <andy-python@hammerhartes.de>
2013-02-12 22:07:47 +0100

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+from setuptools import setup
+
+
+def regenerate_dropin_cache():
+    """Make Twisted regenerate the dropin.cache, if possible. This is
+    necessary because in a site-wide install, dropin.cache cannot be
+    rewritten by normal users.
+    """
+    try:
+        from twisted.plugin import IPlugin, getPlugins
+    except ImportError:
+        pass
+    else:
+        list(getPlugins(IPlugin))
+
+
+setup(
+    name="trumpet",
+    description="The commit announcement IRC bot.",
+    packages=["trumpet", "trumpet.listeners", "trumpet.test","twisted.plugins"],
+    version="0.1",
+    author="Andreas Stührk",
+    author_email="andreas@buffer.io",
+    install_requires=[
+        "twisted"
+    ],
+    zip_safe=False)
+
+
+regenerate_dropin_cache()
